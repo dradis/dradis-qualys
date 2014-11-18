533dffbcbf107bd74109c44de42fbdd636e12964
Jose A. Rilla <jose@rilla.es>
2014-11-18 16:47:07 +0100

    Prepare rspec fixtures and helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,32 @@
+require 'bundler/setup'
+Bundler.setup
+
+require 'dradis-qualys'
+
+RSpec.configure do |config|
+  # Filter which specs to run
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+  # Enable colors
+  config.color_enabled = true
+  # Use the specified formatter
+  config.formatter = :documentation
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
