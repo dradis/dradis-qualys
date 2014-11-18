f2940681d493271fab853faddb6ae5741d1de815
Jose A. Rilla <jose@rilla.es>
2014-11-18 17:41:57 +0100

    Hook engine with Dradis::Plugins and the Upload Manager

--- lib/dradis-qualys.rb
+++ lib/dradis-qualys.rb
@@ -1,2 +1,5 @@
+# hook to the framework base clases
+require 'dradis/plugins'
+
 # Load this add-on's engine
 require 'dradis/plugins/qualys'
--- lib/dradis/plugins/qualys.rb
+++ lib/dradis/plugins/qualys.rb
@@ -1,6 +1,15 @@
 module Dradis
   module Plugins
     module Qualys
+      # This is required while we transition the Upload Manager to use
+      # Dradis::Plugins only
+      module Meta
+        NAME = "Qualys XML upload plugin"
+        EXPECTS = "Qualys XML format."
+        module VERSION
+          include Dradis::Plugins::Qualys::VERSION
+        end
+      end
     end
   end
 end
