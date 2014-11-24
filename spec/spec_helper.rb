dcac53e38e4dfd5a5dd3861764a1674d8f75a85c
Jose A. Rilla <jose@rilla.es>
2014-11-24 21:16:45 +0100

    Cleanup spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,32 +5,9 @@ require 'combustion'
 
 Combustion.initialize!
 
-require 'rspec/rails'
-
 RSpec.configure do |config|
-  # Filter which specs to run
-  config.filter_run :focus => true
-  config.run_all_when_everything_filtered = true
   # Enable colors
-  config.color_enabled = true
+  config.color = true
   # Use the specified formatter
   config.formatter = :documentation
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = false
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
 end
