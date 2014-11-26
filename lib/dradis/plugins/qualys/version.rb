6eca35bc0c08eacfe6c706273ddea4a4c1b590bb
Jose A. Rilla <jose@rilla.es>
2014-11-26 19:12:48 +0100

    Fix typo: s/Nessus/Qualys/

--- lib/dradis/plugins/qualys/version.rb
+++ lib/dradis/plugins/qualys/version.rb
@@ -3,7 +3,7 @@ require_relative 'gem_version'
 module Dradis
   module Plugins
     module Qualys
-      # Returns the version of the currently loaded Nessus as a
+      # Returns the version of the currently loaded Qualys as a
       # <tt>Gem::Version</tt>.
       def self.version
         gem_version
