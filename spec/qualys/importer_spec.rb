f298451f6f47e1c8f0ff88725d309304b0b52dd3
George Millo <georgejulianmillo@gmail.com>
2015-04-20 20:06:12 +0800

    DRYing importer spec

--- spec/qualys/importer_spec.rb
+++ spec/qualys/importer_spec.rb
@@ -41,55 +41,44 @@ describe Dradis::Plugins::Qualys::Importer do
 
   it "creates nodes as needed" do
     # Host node
-    expect(content_service).to receive(:create_node).with(
-      hash_including label: '10.0.155.160'
-    ).once
+    expect_to_create_node_with(label: '10.0.155.160')
+
     # Information gathering node
-    expect(content_service).to receive(:create_node).with(
-      hash_including label: 'infos - Information gathering'
-    ).once
+    expect_to_create_node_with(label: 'infos - Information gathering')
 
     # Services node with its child nodes
-    expect(content_service).to receive(:create_node).with(
-      hash_including label: 'services'
-    ).once
-    expect(content_service).to receive(:create_node).with(
-      hash_including label: 'TCP/IP'
-    ).once
-    expect(content_service).to receive(:create_node).with(
-      hash_including label: 'Web server'
-    ).once
+    expect_to_create_node_with(label: 'services')
+    expect_to_create_node_with(label: 'TCP/IP')
+    expect_to_create_node_with(label: 'Web server')
 
     run_import!
   end
 
 
   it "creates notes as needed" do
-    # Host node
-    expect(content_service).to receive(:create_note) do |args|
-      expect(args[:text]).to include("Basic host info")
-    end
+    # Host node notes
+    expect_to_create_note_with(text: "Basic host info")
 
     # Information gathering node and notes
-    expect(content_service).to receive(:create_note) do |args|
-      expect(args[:text]).to include("DNS Host Name")
-      expect(args[:node].label).to eq("infos - Information gathering")
-    end.once
-    expect(content_service).to receive(:create_note) do |args|
-      expect(args[:text]).to include("Host Scan Time")
-      expect(args[:node].label).to eq("infos - Information gathering")
-    end.once
+    expect_to_create_note_with(
+      text: "DNS Host Name",
+      node_label: "infos - Information gathering"
+    )
+    expect_to_create_note_with(
+      text: "Host Scan Time",
+      node_label: "infos - Information gathering"
+    )
 
     # Child notes of Services node
-    expect(content_service).to receive(:create_note) do |args|
-      expect(args[:text]).to include("Open TCP Services List")
-      expect(args[:node].label).to eq("TCP/IP")
-    end.once
+    expect_to_create_note_with(
+      text: "Open TCP Services List",
+      node_label: "TCP/IP"
+    )
 
-    expect(content_service).to receive(:create_note) do |args|
-      expect(args[:text]).to include("Web Server Version")
-      expect(args[:node].label).to eq("Web server")
-    end.once
+    expect_to_create_note_with(
+      text: "Web Server Version",
+      node_label: "Web server"
+    )
 
     run_import!
   end
@@ -102,39 +91,39 @@ describe Dradis::Plugins::Qualys::Importer do
   # Each one should create 1 issue and 1 evidence
 
   it "creates issues from vulns" do
-    expect(content_service).to receive(:create_issue) do |args|
-      expect(args[:text]).to include("Sequence Number Approximation Based Denial of Service")
-    end.once
+    expect_to_create_issue_with(
+      text: "Sequence Number Approximation Based Denial of Service"
+    )
 
-    expect(content_service).to receive(:create_issue) do |args|
-      expect(args[:text]).to include("Apache 1.3 HTTP Server Expect Header Cross-Site Scripting")
-    end.once
+    expect_to_create_issue_with(
+      text: "Apache 1.3 HTTP Server Expect Header Cross-Site Scripting"
+    )
 
-    expect(content_service).to receive(:create_issue) do |args|
-      expect(args[:text]).to include("Apache Web Server ETag Header Information Disclosure Weakness")
-    end.once
+    expect_to_create_issue_with(
+      text: "Apache Web Server ETag Header Information Disclosure Weakness"
+    )
 
     run_import!
   end
 
   it "creates evidence from vulns" do
-    expect(content_service).to receive(:create_evidence) do |args|
-      expect(args[:content]).to include("Tested on port 80 with an injected SYN/RST offset by 16 bytes.")
-      expect(args[:issue].text).to include("Sequence Number Approximation Based Denial of Service")
-      expect(args[:node].label).to eq("10.0.155.160")
-    end.once
+    expect_to_create_evidence_with(
+      content: "Tested on port 80 with an injected SYN/RST offset by 16 bytes.",
+      issue: "Sequence Number Approximation Based Denial of Service",
+      node_label: "10.0.155.160"
+    )
 
-    expect(content_service).to receive(:create_evidence) do |args|
-      expect(args[:content]).to include("The expectation given in the Expect request-header")
-      expect(args[:issue].text).to include("Apache 1.3 HTTP Server Expect Header Cross-Site Scripting")
-      expect(args[:node].label).to eq("10.0.155.160")
-    end.once
+    expect_to_create_evidence_with(
+      content: "The expectation given in the Expect request-header",
+      issue: "Apache 1.3 HTTP Server Expect Header Cross-Site Scripting",
+      node_label: "10.0.155.160"
+    )
 
-    expect(content_service).to receive(:create_evidence) do |args|
-      expect(args[:content]).to include("bee-4f12-00794aef")
-      expect(args[:issue].text).to include("Apache Web Server ETag Header Information Disclosure Weakness")
-      expect(args[:node].label).to eq("10.0.155.160")
-    end.once
+    expect_to_create_evidence_with(
+      content: "bee-4f12-00794aef",
+      issue: "Apache Web Server ETag Header Information Disclosure Weakness",
+      node_label: "10.0.155.160"
+    )
 
     run_import!
   end
@@ -148,27 +137,54 @@ describe Dradis::Plugins::Qualys::Importer do
 
     it "detects an issue without a RESULT element and applies (n/a)" do
       # 1 node should be created:
-      expect(content_service).to receive(:create_node).with(
-        hash_including label: '10.0.155.160'
-      ).once
+      expect_to_create_node_with(label: '10.0.155.160')
 
       # There is 1 vuln in total:
       #   - TCP/IP: Sequence number in both hosts
       # Each one should create 1 issue and 1 evidence
-      expect(content_service).to receive(:create_issue) do |args|
-        expect(args[:text]).to include("Sequence Number Approximation Based Denial of Service")
-        OpenStruct.new(args)
-      end.once
+      expect_to_create_issue_with(
+        text: "Sequence Number Approximation Based Denial of Service"
+      )
 
-      expect(content_service).to receive(:create_evidence) do |args|
-        expect(args[:content]).to include("n/a")
-        expect(args[:issue].text).to include("Sequence Number Approximation Based Denial of Service")
-        expect(args[:node].label).to eq("10.0.155.160")
-      end.once
+      expect_to_create_evidence_with(
+        content: "n/a",
+        issue: "Sequence Number Approximation Based Denial of Service",
+        node_label: "10.0.155.160"
+      )
 
       run_import!
     end
   end
+
+
+  def expect_to_create_node_with(label:)
+    expect(content_service).to receive(:create_node).with(
+      hash_including label: label
+    ).once
+  end
+
+  def expect_to_create_note_with(node_label: nil, text:)
+    expect(content_service).to receive(:create_note) do |args|
+      expect(args[:text]).to include text
+      expect(args[:node].label).to eq node_label unless node_label.nil?
+    end.once
+  end
+
+  def expect_to_create_issue_with(text:)
+    expect(content_service).to receive(:create_issue) do |args|
+      expect(args[:text]).to include text
+      OpenStruct.new(args)
+    end.once
+  end
+
+  def expect_to_create_evidence_with(content:, issue:, node_label:)
+    expect(content_service).to receive(:create_evidence) do |args|
+      expect(args[:content]).to include content
+      expect(args[:issue].text).to include issue
+      expect(args[:node].label).to eq node_label
+    end.once
+  end
+
 
 end
 
