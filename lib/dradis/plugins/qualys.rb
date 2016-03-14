8ec1662c5ea23955420e5447775d5753eed773fa
Daniel Martin <etd-github@nomejortu.com>
2016-03-14 12:53:43 +0100

    We no longer need a Meta module

--- lib/dradis/plugins/qualys.rb
+++ lib/dradis/plugins/qualys.rb
@@ -9,19 +9,3 @@ require 'dradis/plugins/qualys/engine'
 require 'dradis/plugins/qualys/field_processor'
 require 'dradis/plugins/qualys/importer'
 require 'dradis/plugins/qualys/version'
-
-# This is required while we transition the Upload Manager to use
-# Dradis::Plugins only
-module Dradis
-  module Plugins
-    module Qualys
-      module Meta
-        NAME = "Qualys XML upload plugin"
-        EXPECTS = "Qualys XML format."
-        module VERSION
-          include Dradis::Plugins::Qualys::VERSION
-        end
-      end
-    end
-  end
-end
