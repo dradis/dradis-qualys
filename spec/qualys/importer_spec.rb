2ac10ef98de1826135000e849c1829cbefda8ac6
Rachael Korinek <rachael.korinek@gmail.com>
2017-06-07 17:29:24 -0500

    Update specs to accomodate INFOS/SERVICES/PRACTICES being imported as Issues instead of Nodes/Notes

--- spec/qualys/importer_spec.rb
+++ spec/qualys/importer_spec.rb
@@ -4,7 +4,7 @@ require "ostruct"
 describe Dradis::Plugins::Qualys::Importer do
   let(:plugin) { Dradis::Plugins::Qualys }
 
-  let(:content_service)  { Dradis::Plugins::ContentService.new(plugin: plugin) }
+  let(:content_service)  { Dradis::Plugins::ContentService::Base.new(plugin: plugin) }
   let(:template_service) { Dradis::Plugins::TemplateService.new(plugin: plugin) }
 
   let(:importer) {
@@ -33,8 +33,6 @@ describe Dradis::Plugins::Qualys::Importer do
 
   let(:example_xml) { 'spec/fixtures/files/simple.xml' }
 
-  pending "collapses INFOS|SERVICES|VULNS|PRACTICES node if only a single element is found"
-
   def run_import!
     importer.import(file: example_xml)
   end
@@ -42,14 +40,6 @@ describe Dradis::Plugins::Qualys::Importer do
   it "creates nodes as needed" do
     # Host node
     expect_to_create_node_with(label: '10.0.155.160')
-
-    # Information gathering node
-    expect_to_create_node_with(label: 'infos - Information gathering')
-
-    # Services node with its child nodes
-    expect_to_create_node_with(label: 'services')
-    expect_to_create_node_with(label: 'TCP/IP')
-    expect_to_create_node_with(label: 'Web server')
 
     run_import!
   end
@@ -59,68 +49,88 @@ describe Dradis::Plugins::Qualys::Importer do
     # Host node notes
     expect_to_create_note_with(text: "Basic host info")
 
-    # Information gathering node and notes
-    expect_to_create_note_with(
-      text: "DNS Host Name",
-      node_label: "infos - Information gathering"
-    )
-    expect_to_create_note_with(
-      text: "Host Scan Time",
-      node_label: "infos - Information gathering"
-    )
-
-    # Child notes of Services node
-    expect_to_create_note_with(
-      text: "Open TCP Services List",
-      node_label: "TCP/IP"
-    )
-
-    expect_to_create_note_with(
-      text: "Web Server Version",
-      node_label: "Web server"
-    )
-
     run_import!
   end
 
   # Issues and evidences from vulns
-  # There are 3 vulns in total:
+  # There are 7 vulns/infos/services in total:
+  #   - DNS Host Name
+  #   - Host Scan Time
+  #   - Open TCP Services List
+  #   - Web Server Version
   #   - TCP/IP: Sequence number in both hosts
   #   - Web server: Apache 1.3
   #   - Web server: ETag
-  # Each one should create 1 issue and 1 evidence
 
   it "creates issues from vulns" do
     expect_to_create_issue_with(
-      text: "Sequence Number Approximation Based Denial of Service"
+      text: "DNS Host Name"
+    )
+
+    expect_to_create_issue_with(
+      text: "Host Scan Time"
+    )
+
+    expect_to_create_issue_with(
+      text: "Open TCP Services List"
+    )
+
+    expect_to_create_issue_with(
+      text: "Web Server Version"
+    )
+
+    expect_to_create_issue_with(
+      text: "TCP Sequence Number Approximation Based Denial of Service"
     )
 
     expect_to_create_issue_with(
       text: "Apache 1.3 HTTP Server Expect Header Cross-Site Scripting"
     )
-
+    
     expect_to_create_issue_with(
       text: "Apache Web Server ETag Header Information Disclosure Weakness"
     )
-
+    
     run_import!
   end
 
   it "creates evidence from vulns" do
     expect_to_create_evidence_with(
-      content: "Tested on port 80 with an injected SYN/RST offset by 16 bytes.",
-      issue: "Sequence Number Approximation Based Denial of Service",
+      content: "IP address\tHost name\n10.0.155.160\tNo registered hostname\n",
+      issue: "DNS Host Name",
       node_label: "10.0.155.160"
     )
 
     expect_to_create_evidence_with(
-      content: "The expectation given in the Expect request-header",
+      content: "Scan duration: 5445 seconds\n\nStart time: Fri, Dec 20 2011, 17:38:59 GMT\n\nEnd time: Fri, Dec 20 2011, 19:09:44 GMT",
+      issue: "Host Scan Time",
+      node_label: "10.0.155.160"
+    )
+
+    expect_to_create_evidence_with(
+      content: "\tDescription\tService Detected\tOS On Redirected Port\n80\twww\tWorld Wide Web HTTP\thttp",
+      issue: "Open TCP Services List",
+      node_label: "10.0.155.160"
+    )
+
+    expect_to_create_evidence_with(
+      content: "Server Version\tServer Banner\nApache 1.3\tApache",
+      issue: "Web Server Version",
+      node_label: "10.0.155.160"
+    )
+
+    expect_to_create_evidence_with(
+      content: "Tested on port 80 with an injected SYN/RST offset by 16 bytes.",
+      issue: "TCP Sequence Number Approximation Based Denial of Service",
+      node_label: "10.0.155.160"
+    )
+    expect_to_create_evidence_with(
+      content: "HTTP/1.1 417 Expectation Failed\nDate: Fri, 20 Dec 2011 19:05:57 GMT",
       issue: "Apache 1.3 HTTP Server Expect Header Cross-Site Scripting",
       node_label: "10.0.155.160"
     )
-
     expect_to_create_evidence_with(
-      content: "bee-4f12-00794aef",
+      content: "3bee-4f12-00794aef",
       issue: "Apache Web Server ETag Header Information Disclosure Weakness",
       node_label: "10.0.155.160"
     )
