a2198b29224f2a2675baa0d5ed2311d1c5fac4a7
Aaron Manaloto <mail@aapomm.io>
2022-01-12 16:49:09 +0800

    Add upload_was task for uploading Qualys WAS XML

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -18,4 +18,18 @@ class QualysTasks < Thor
     importer.import(file: file_path)
   end
 
+  desc "upload_was FILE", "upload Qualys WAS XML results"
+  def upload_was(file_path)
+    require 'config/environment'
+
+    unless File.exists?(file_path)
+      $stderr.puts "** the file [#{file_path}] does not exist"
+      exit -1
+    end
+
+    detect_and_set_project_scope
+
+    importer = Dradis::Plugins::Qualys::WAS::Importer.new(task_options)
+    importer.import(file: file_path)
+  end
 end
