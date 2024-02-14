befc284da55c89eb246464a44ca79a80e2e9748e
Matt Budz <matt.budz@protonmail.com>
2024-02-14 17:12:31 +0100

    update hash values to help with parsing on upload

--- app/services/dradis/plugins/qualys/mapping_service.rb
+++ app/services/dradis/plugins/qualys/mapping_service.rb
@@ -3,63 +3,63 @@ module Dradis::Plugins::Qualys
     def default_mapping
       {
         'asset-evidence' => {
-          'Result' => '{{ asset-evidence.result }}',
-          'Status' => '{{ asset-evidence.vuln_status }}',
-          'SSL' => '{{ asset-evidence.ssl }}',
-          'CVSSv3.Final' => '{{ asset-evidence.cvss_final }}'
+          'Result' => '{{ qualys[asset-evidence.result] }}',
+          'Status' => '{{ qualys[asset-evidence.vuln_status] }}',
+          'SSL' => '{{ qualys[asset-evidence.ssl] }}',
+          'CVSSv3.Final' => '{{ qualys[asset-evidence.cvss_final] }}'
         },
         'asset-issue' => {
-          'Title' => '{{ asset-issue.title }}',
-          'Severity' => '{{ asset-issue.severity }}',
-          'Categories' => 'Category: {{ asset-issue.category }}',
-          'CVSSv3.BaseScore' => '{{ asset-issue.cvss3_base }}',
-          'CVSSv3.TemporalScore' => '{{ asset-issue.cvss3_temporal }}',
-          'Threat' => '{{ asset-issue.threat }}',
-          'Impact' => '{{ asset-issue.impact }}',
-          'Solution' => '{{ asset-issue.solution }}'
+          'Title' => '{{ qualys[asset-issue.title] }}',
+          'Severity' => '{{ qualys[asset-issue.severity] }}',
+          'Categories' => 'Category: {{ qualys[asset-issue.category] }}',
+          'CVSSv3.BaseScore' => '{{ qualys[asset-issue.cvss3_base] }}',
+          'CVSSv3.TemporalScore' => '{{ qualys[asset-issue.cvss3_temporal] }}',
+          'Threat' => '{{ qualys[asset-issue.threat] }}',
+          'Impact' => '{{ qualys[asset-issue.impact] }}',
+          'Solution' => '{{ qualys[asset-issue.solution] }}'
         },
         'element' => {
-          'Title' => '{{ element.title }}',
-          'Severity' => '{{ element.severity }}',
-          'CVE' => '{{ element.cveid }}',
-          'CVSS' => "Base: {{ element.cvss_base }}\n
-                    Temporal: {{ element.cvss_temporal }}",
-          'Diagnosis' => '{{ element.diagnosis }}',
-          'Consequence' => '{{ element.consequence }}',
-          'Solution' => '{{ element.solution }}',
-          'Result' => '{{ element.result }}',
-          'CVEList' => '{{ element.cve_id_list }}',
-          'QualysCollection' => '{{ element.qualys_collection }}'
+          'Title' => '{{ qualys[element.title] }}',
+          'Severity' => '{{ qualys[element.severity] }}',
+          'CVE' => '{{ qualys[element.cveid] }}',
+          'CVSS' => "Base: {{ qualys[element.cvss_base] }}\n
+                    Temporal: {{ qualys[element.cvss_temporal] }}",
+          'Diagnosis' => '{{ qualys[element.diagnosis] }}',
+          'Consequence' => '{{ qualys[element.consequence] }}',
+          'Solution' => '{{ qualys[element.solution] }}',
+          'Result' => '{{ qualys[element.result] }}',
+          'CVEList' => '{{ qualys[element.cve_id_list] }}',
+          'QualysCollection' => '{{ qualys[element.qualys_collection] }}'
         },
         'evidence' => {
-          'Category' => '{{ evidence.cat_value }}',
-          'Protocol' => '{{ evidence.cat_protocol }}',
-          'Port' => '{{ evidence.cat_port }}',
-          'Output' => '{{ evidence.result }}'
+          'Category' => '{{ qualys[evidence.cat_value] }}',
+          'Protocol' => '{{ qualys[evidence.cat_protocol] }}',
+          'Port' => '{{ qualys[evidence.cat_port] }}',
+          'Output' => '{{ qualys[evidence.result] }}'
         },
         'was-evidence' => {
-          'Location' => '{{ was-evidence.url }}',
+          'Location' => '{{ qualys[was-evidence.url] }}',
           'Output' => "*Request*\n\n
-                      Method: {{ was-evidence.request_method }}\n
-                      URL: {{ was-evidence.request_url }}\n\n
-                      bc.. {{ was-evidence.request_headers }}\n\n
+                      Method: {{ qualys[was-evidence.request_method] }}\n
+                      URL: {{ qualys[was-evidence.request_url] }}\n\n
+                      bc.. {{ qualys[was-evidence.request_headers] }}\n\n
                       p. *Response*\n\n
-                      Evidence: {{ was-evidence.response_evidence }}\n\n
-                      bc.. {{ was-evidence.response_contents }}"
+                      Evidence: {{ qualys[was-evidence.response_evidence] }}\n\n
+                      bc.. {{ qualys[was-evidence.response_contents] }}"
         },
         'was-issue' => {
-          'Title' => '{{ was-issue.title }}',
-          'Severity' => '{{ was-issue.severity }}',
-          'Categories' => "Category: {{ was-issue.category }}\n
-                          Group: {{ was-issue.group }}\n
-                          OWASP: {{ was-issue.owasp }}\n
-                          CWE: {{ was-issue.cwe }}",
-          'CVSSv3.Vector' => '{{ was-issue.cvss3_vector }}',
-          'CVSSv3.BaseScore' => '{{ was-issue.cvss3_base }}',
-          'CVSSv3.TemporalScore' => '{{ was-issue.cvss3_temporal }}',
-          'Description' => '{{ was-issue.description }}',
-          'Impact' => '{{ was-issue.impact }}',
-          'Solution' => '{{ was-issue.solution }}'
+          'Title' => '{{ qualys[was-issue.title] }}',
+          'Severity' => '{{ qualys[was-issue.severity] }}',
+          'Categories' => "Category: {{ qualys[was-issue.category] }}\n
+                          Group: {{ qualys[was-issue.group] }}\n
+                          OWASP: {{ qualys[was-issue.owasp] }}\n
+                          CWE: {{ qualys[was-issue.cwe] }}",
+          'CVSSv3.Vector' => '{{ qualys[was-issue.cvss3_vector] }}',
+          'CVSSv3.BaseScore' => '{{ qualys[was-issue.cvss3_base] }}',
+          'CVSSv3.TemporalScore' => '{{ qualys[was-issue.cvss3_temporal] }}',
+          'Description' => '{{ qualys[was-issue.description] }}',
+          'Impact' => '{{ qualys[was-issue.impact] }}',
+          'Solution' => '{{ qualys[was-issue.solution] }}'
         }
       }
     end
